0ef17526ee28ed0b11f259c2505cb6bd73be8d8b
ACT-C2-03 <ACT-C2-03@DESKTOP-I4SCAC5>
2025-06-13 11:22:03 +0530

    Identifier , day1 program

---
+++ C2TC_CoreJavaPrograms/src/day1/Identifier.java
@@ -0,0 +1,15 @@
+package day1;
+public class Identifier{
+
+	public static void main(String[] args) {
+
+		int $number = 20;
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Anandh"; 
+		System.out.println("value is : "+ studentName);
+		
+	
+	}
+
+}
