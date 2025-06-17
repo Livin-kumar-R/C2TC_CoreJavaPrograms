f9fcf0a15a3619c6c2c0f465bee9056fe8b60457
ACT-C2-03 <ACT-C2-03@DESKTOP-I4SCAC5>
2025-06-17 09:17:20 +0530

    Scanner demo

---
+++ C2TC_CoreJavaPrograms/src/day3/ScannerDemo.java
@@ -0,0 +1,23 @@
+package day3;
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your Name: ");
+		String name = sc.next();
+		System.out.print("Enter your age: ");
+		int age = sc.nextInt();
+		System.out.print("Enter your salary: ");
+		double salary = sc.nextDouble();
+		
+		
+		System.out.println("Name :" + name);
+		System.out.println("Age: " + age);
+		System.out.println("Salary: " + salary);
+		
+
+	}
+
+}
